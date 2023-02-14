ddeb3419ef914c77aa2052ab9a9c341e8e278567
Tanzil-Hudson <113275165+Tanzil-Hudson@users.noreply.github.com>
2023-02-14 15:09:19 -0500

    Feb 14

---
+++ WishList/Views/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public Action Index(IActionResult actionResult)
+        {
+            return View();
+        }
+    }
+}
