a389d983cb1263fdc82eaf0ea2ae65f0a73d5e33
Tanzil-Hudson <113275165+Tanzil-Hudson@users.noreply.github.com>
2023-02-19 12:59:06 -0500

    Update ApplicationDbContext.cs

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -5,6 +5,9 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
 
+        }
     }
 }
